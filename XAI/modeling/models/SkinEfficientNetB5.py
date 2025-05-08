372bd6e63faff53cd32ae4aeaf2617e83f58c6ed
Mahmoud El Gohary <147744824+MahmoudEl-Gohary@users.noreply.github.com>
2025-05-09 02:23:28 +0300

    Update SkinEfficientNetB5.py

--- XAI/modeling/models/SkinEfficientNetB5.py
+++ XAI/modeling/models/SkinEfficientNetB5.py
@@ -6,7 +6,7 @@ from XAI.config import NUM_CLASSES
 from XAI.modeling.models.Base_Model import BaseModel
 
 class SkinEfficientNetB5(BaseModel):
-    def __init__(self, num_classes=NUM_CLASSES, freeze_backbone=True):
+    def __init__(self, num_classes=NUM_CLASSES, freeze_backbone=False):
         super(SkinEfficientNetB5, self).__init__()
 
         self.backbone = efficientnet_b5(weights=EfficientNet_B5_Weights.IMAGENET1K_V1)
